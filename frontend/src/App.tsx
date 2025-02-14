b3edb7eef0c75e3f4149ce401ef571e546a6d915
sebastian castiblanco <scastiblanco2003gmail.com>
2025-02-14 00:45:15 -0500

    rutas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/landing'; // Ahora Landing será la invitación
 import ListaInvitados from './components/GuestList'; // Página con los links personalizados
 
 function App() {
   return (
-    <Router basename="/invitacion-samuel">
+    <Router>
       <Routes>
-        {/* Lista de invitados */}
         <Route path="/lista-invitados" element={<ListaInvitados />} />
-        {/* Invitación personalizada */}
         <Route path="/" element={<Landing />} />
       </Routes>
     </Router>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://castlewhites.github.io/invitacion-samuel",
+  base: "/invitacion-samuel/",
   server: {
     proxy: {
       '/asistencia': {
